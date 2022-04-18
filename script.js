f911da883edd8799da61419a5fd02b03030e3550
Geraldine Ezeocha <gerriezeocha@Geraldines-Mac-mini.local>
2022-04-18 18:32:57 -0500

    final styling

--- script.js
+++ script.js
@@ -8,28 +8,28 @@ fetch('./data.json')
     .then(jsondata => { 
         for (var i = 0; i < cards.length; i++) {
             titles[i].innerHTML = jsondata[i].title;
-            hours[i].innerHTML = jsondata[i].timeframes.daily.current;
+            hours[i].innerHTML = `${jsondata[i].timeframes.daily.current}hrs`;
             prevs[i].innerHTML = `Last Week - ${jsondata[i].timeframes.daily.previous}hrs`;
         }
         
         daily.addEventListener('click', () => {
             for (var i = 0; i < cards.length; i++) {
                 titles[i].innerHTML = jsondata[i].title;
-                hours[i].innerHTML = jsondata[i].timeframes.daily.current;
+                hours[i].innerHTML = `${jsondata[i].timeframes.daily.current}hrs`;
                 prevs[i].innerHTML = `Last Week - ${jsondata[i].timeframes.daily.previous}hrs`;
             }
         });
         weekly.addEventListener('click', () => {
             for (var i = 0; i < cards.length; i++) {
                 titles[i].innerHTML = jsondata[i].title;
-                hours[i].innerHTML = jsondata[i].timeframes.weekly.current;
+                hours[i].innerHTML = `${jsondata[i].timeframes.weekly.current}hrs`;
                 prevs[i].innerHTML = `Last Week - ${jsondata[i].timeframes.weekly.previous}hrs`;
             }
         });
         monthly.addEventListener('click', () => {
             for (var i = 0; i < cards.length; i++) {
                 titles[i].innerHTML = jsondata[i].title;
-                hours[i].innerHTML = jsondata[i].timeframes.monthly.current;
+                hours[i].innerHTML = `${jsondata[i].timeframes.monthly.current}hrs`;
                 prevs[i].innerHTML = `Last Week - ${jsondata[i].timeframes.monthly.previous}hrs`;
             }
         });
